e1be3d375fdc0d3d04a3fb764a92b01674702cd8
Hristiyan33 <36618474+Hristiyan33@users.noreply.github.com>
2018-02-26 08:02:21 +0200

    Add files via upload

---
+++ Masiv 1.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int sum=0;
+float avg;
+int array[8]={3,7,15,35,75,85,90,-3};
+for(int i=0;i<8;i++)
+{
+sum+=array[i];
+}
+avg=sum/8.0;
+cout<<"Average:"<<avg;
+    return 0;
+}
